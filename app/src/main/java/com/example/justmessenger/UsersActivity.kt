2b44e789a5b3e5682ea869766abbed1928056cfb
Bulator <46053514+Bulator@users.noreply.github.com>
2021-04-05 14:44:20 +0300

    Добавил Toolbar и кнопку для выхода из аккаунта

--- app/src/main/java/com/example/justmessenger/UsersActivity.kt
+++ app/src/main/java/com/example/justmessenger/UsersActivity.kt
@@ -2,10 +2,28 @@ package com.example.justmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.justmessenger.databinding.ActivityUsersBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class UsersActivity : AppCompatActivity() {
+
+    lateinit private var binding: ActivityUsersBinding
+    lateinit private var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_users)
+        binding = ActivityUsersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.settingsImgbtn.setOnClickListener {
+            auth.signOut()
+            Toast.makeText(this, "You have signed out.", Toast.LENGTH_SHORT).show()
+            onDestroy()
+        }
     }
 }
